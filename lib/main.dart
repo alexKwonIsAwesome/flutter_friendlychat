2273e39be047a0f320f1d7c5e6cc33211207de58
alexKwonIsAwesome <khwalex@gmail.com>
2019-09-15 00:17:05 +0900

    Add text input for chat message input ui

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,49 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+        data: IconThemeData(color: Theme.of(context).accentColor),
+        child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: <Widget>[
+                Flexible(
+                  child: TextField(
+                    controller: _textController,
+                    onSubmitted: _handleSubmitted,
+                    decoration:
+                        InputDecoration.collapsed(hintText: "Send a message"),
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.symmetric(horizontal: 4.0),
+                    child: IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: () => _handleSubmitted(_textController.text),
+                    ))
+              ],
+            )));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Friendlychat'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Friendlychat'),
+        ),
+        body: _buildTextComposer());
   }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  ChatScreenState createState() => ChatScreenState();
 }
