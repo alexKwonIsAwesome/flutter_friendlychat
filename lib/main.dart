b8a49075869741ec9f400b9913bc7e5ead44d0ad
alexKwonIsAwesome <khwalex@gmail.com>
2019-09-15 08:23:28 +0900

    Add fade animation effect on message input

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,20 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreenState extends State<ChatScreen> {
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = TextEditingController();
 
   void _handleSubmitted(String text) {
     _textController.clear();
-    ChatMessage message = ChatMessage(text: text);
+    ChatMessage message = ChatMessage(
+        text: text,
+        animationController: AnimationController(
+            duration: Duration(milliseconds: 700), vsync: this));
     setState(() {
       _messages.insert(0, message);
     });
+    message.animationController.forward();
   }
 
   Widget _buildTextComposer() {
@@ -74,6 +78,14 @@ class ChatScreenState extends State<ChatScreen> {
           ],
         ));
   }
+
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages) {
+      message.animationController.dispose();
+    }
+    super.dispose();
+  }
 }
 
 class ChatScreen extends StatefulWidget {
@@ -84,29 +96,34 @@ class ChatScreen extends StatefulWidget {
 const String _name = 'Your name';
 
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.text});
+  ChatMessage({this.text, this.animationController});
 
   final String text;
+  final AnimationController animationController;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: const EdgeInsets.symmetric(vertical: 10.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-                margin: const EdgeInsets.only(right: 16.0),
-                child: CircleAvatar(child: Text(_name[0]))),
-            Column(
+    return FadeTransition(
+        opacity:
+            CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+        child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text(_name, style: Theme.of(context).textTheme.subhead),
                 Container(
-                    margin: const EdgeInsets.only(top: 5.0), child: Text(text))
+                    margin: const EdgeInsets.only(right: 16.0),
+                    child: CircleAvatar(child: Text(_name[0]))),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(_name, style: Theme.of(context).textTheme.subhead),
+                    Container(
+                        margin: const EdgeInsets.only(top: 5.0),
+                        child: Text(text))
+                  ],
+                )
               ],
-            )
-          ],
-        ));
+            )));
   }
 }
