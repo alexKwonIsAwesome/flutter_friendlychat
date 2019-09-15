cc55385892fa8a2b054299e3f6b139f030142d8a
alexKwonIsAwesome <khwalex@gmail.com>
2019-09-15 09:02:38 +0900

    Add platform specific theme and stylings

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() => runApp(FriendlychatApp());
+
+final ThemeData iosTheme = ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light);
+
+final ThemeData andriodTheme = ThemeData(
+    primarySwatch: Colors.purple, accentColor: Colors.orangeAccent[400]);
 
 class FriendlychatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Friendlychat", home: ChatScreen());
+    return MaterialApp(
+        title: "Friendlychat",
+        theme: defaultTargetPlatform == TargetPlatform.iOS
+            ? iosTheme
+            : andriodTheme,
+        home: ChatScreen());
   }
 }
 
@@ -54,12 +69,19 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
                 ),
                 Container(
                     margin: EdgeInsets.symmetric(horizontal: 4.0),
-                    child: IconButton(
-                      icon: Icon(Icons.send),
-                      onPressed: () => _isComposing
-                          ? _handleSubmitted(_textController.text)
-                          : null,
-                    ))
+                    child: Theme.of(context).platform == TargetPlatform.iOS
+                        ? CupertinoButton(
+                            child: Text('Send'),
+                            onPressed: () => _isComposing
+                                ? _handleSubmitted(_textController.text)
+                                : null,
+                          )
+                        : IconButton(
+                            icon: Icon(Icons.send),
+                            onPressed: () => _isComposing
+                                ? _handleSubmitted(_textController.text)
+                                : null,
+                          ))
               ],
             )));
   }
@@ -69,27 +91,35 @@ class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
     return Scaffold(
         appBar: AppBar(
           title: Text('Friendlychat'),
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
         ),
-        body: Column(
-          children: <Widget>[
-            Flexible(
-              child: ListView.builder(
-                padding: EdgeInsets.all(8.0),
-                reverse: true,
-                itemBuilder: (_, int index) => _messages[index],
-                itemCount: _messages.length,
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                child: ListView.builder(
+                  padding: EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemBuilder: (_, int index) => _messages[index],
+                  itemCount: _messages.length,
+                ),
               ),
-            ),
-            Divider(
-              height: 1.0,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).cardColor,
+              Divider(
+                height: 1.0,
               ),
-              child: _buildTextComposer(),
-            )
-          ],
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).cardColor,
+                ),
+                child: _buildTextComposer(),
+              )
+            ],
+          ),
+          decoration: Theme.of(context).platform == TargetPlatform.iOS
+              ? BoxDecoration(
+                  border: Border(top: BorderSide(color: Colors.grey[200])))
+              : null,
         ));
   }
 
